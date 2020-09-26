f2880c5ea182800657cc98384ac4dfcbbc70a10d
Undulate-C <66703759+Undulate-C@users.noreply.github.com>
2020-09-26 11:52:32 -0400

    Add files via upload

---
+++ playMusic.java
@@ -0,0 +1,28 @@
+import java.io.File;
+
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+import javax.swing.JOptionPane;
+
+public class playMusic {
+	void MusicPlayer(String musicLocation) {
+		try {
+			File musicPath = new File(musicLocation);
+			 
+			if (musicPath.exists()) {
+				AudioInputStream audioInput = AudioSystem.getAudioInputStream(musicPath);
+				Clip music = AudioSystem.getClip();
+				music.open(audioInput);
+				music.start();
+				music.loop(music.LOOP_CONTINUOUSLY);
+				
+				JOptionPane.showMessageDialog(null, "click me to mute!");
+			}else {
+				System.out.println("File Not Found");
+			}
+		}catch (Exception ex) {
+			ex.printStackTrace();
+		}
+	}
+}
