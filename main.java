a3f8eedb147efbf54bddf0fbf538f2261f248904
Undulate-C <66703759+Undulate-C@users.noreply.github.com>
2020-09-25 20:32:54 -0400

    Add files via upload

---
+++ main.java
@@ -0,0 +1,10 @@
+
+public class main {
+
+	public static void main(String[] args) {
+		playMusic backgroundMusic = new playMusic();
+		backgroundMusic.MusicPlayer("C:\\Users\\ps102\\Desktop\\test.wav");
+		
+	}
+
+}
---
+++ playMusic.java
@@ -0,0 +1,28 @@
+import java.io.File;
+
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+
+public class playMusic {
+	void MusicPlayer(String musicLocation) {
+		try {
+			File musicPath = new File(musicLocation);
+			 
+			if (musicPath.exists()) {
+				AudioInputStream audioInput = AudioSystem.getAudioInputStream(musicPath);
+				Clip music = AudioSystem.getClip();
+				music.open(audioInput);
+				music.start();
+				music.loop(music.LOOP_CONTINUOUSLY);
+				
+				while(true) {
+				}
+			}else {
+				System.out.println("File Not Found");
+			}
+		}catch (Exception ex) {
+			ex.printStackTrace();
+		}
+	}
+}
