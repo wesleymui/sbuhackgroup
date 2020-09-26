5c422d733fab068477a9dac9269cd45eb73e8274
Ashley Zhao <52079959+ashleyz-hchs@users.noreply.github.com>
2020-09-26 15:09:34 -0400

    Update main.java

--- main.java
+++ main.java
@@ -1,10 +1,10 @@
-
 public class main {
+    
 
-	public static void main(String[] args) {
-		playMusic backgroundMusic = new playMusic();
-		backgroundMusic.MusicPlayer("test.wav");
-		
-	}
+    public static void main(String[] args) {
+//        playMusic backgroundMusic = new playMusic();
+//        backgroundMusic.MusicPlayer("test.wav");
+        new GUI();
+        Fox newGame = new Fox("name", 1);
 
-}
+    }
