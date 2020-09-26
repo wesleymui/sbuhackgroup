2f4d2a991e2f3c82b74f515d986010e01d4440b9
Undulate-C <66703759+Undulate-C@users.noreply.github.com>
2020-09-25 20:49:03 -0400

    Update main.java

--- main.java
+++ main.java
@@ -3,7 +3,7 @@ public class main {
 
 	public static void main(String[] args) {
 		playMusic backgroundMusic = new playMusic();
-		backgroundMusic.MusicPlayer("C:\\Users\\ps102\\Desktop\\test.wav");
+		backgroundMusic.MusicPlayer("test.wav");
 		
 	}
 
