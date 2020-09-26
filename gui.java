ee412c01bf804ff7474ae384d1613906961e83b9
wesleymui <68392398+wesleymui@users.noreply.github.com>
2020-09-26 13:00:30 -0400

    gui update

--- gui.java
+++ gui.java
@@ -1,13 +1,47 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-import javax.swing.JFrame;
+public class GUI implements ActionListener {
+    private int count = 0;
+    private JLabel label;
+    private JFrame frame;
+    private JPanel panel;
+    private ImageIcon image1;
 
-public class GUI {
+    public GUI() {
+        frame = new JFrame();
 
-  public GUI() {
-    JFrame frame = new JFrame();
-    JPanel panel = new JPanel();
-    
+        JButton button  = new JButton("Click me");
+        button.addActionListener(this);
 
-  }
+        frame.setLayout(new FlowLayout());
 
+        image1 = new ImageIcon(getClass().getResource("background.png"));
+        frame.add(new JLabel(image1));
+
+        label = new JLabel("Number of clicks: 0");
+
+        panel = new JPanel();
+        panel.setBorder(BorderFactory.createEmptyBorder(200, 400, 200, 400));
+        panel.setLayout(new GridLayout(10, 10));
+        panel.add(button);
+        panel.add(label);
+
+        frame.add(panel, BorderLayout.CENTER);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setTitle("Fox in a Hole");
+        frame.pack();
+        frame.setVisible(true);
+    }
+    public static void main(String[] args) {
+        new GUI();
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        count++;
+        label.setText("Number of clicks: " + count);
+    }
 }
